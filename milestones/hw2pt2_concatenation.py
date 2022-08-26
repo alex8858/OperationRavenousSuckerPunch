3012c3020e609e1bf13f4e2a3edef5009e850c1f
Alex Guay <alexguay@Alexs-MacBook-Pro.local>
2022-08-26 06:38:17 -0600

    Added Data.txt file

--- milestones/hw2pt2_concatenation.py
+++ milestones/hw2pt2_concatenation.py
@@ -1,10 +1,13 @@
 ### will run script with concatenated URL
-
-###Need to still make it into a .txt file
-
-
+###Import Libraries
 from bs4 import BeautifulSoup
 import requests
+
+
+###Create .txt file
+
+f = open("Data.txt","w+", encoding='utf-8', newline='') 
+
 
 links = []
 r = requests.get('https://samples.adsbexchange.com/readsb-hist/2022/05/01/')
@@ -14,11 +17,10 @@ for link in soup.find_all('a'):
     links.append(link.get('href'))
 
 for link in links:
-    print('https://samples.adsbexchange.com/readsb-hist/2022/05/01/'+link)
+     print('https://samples.adsbexchange.com/readsb-hist/2022/05/01/'+link)
 
 print(len(links))
-
-
-
+f.write ('  n/')
+f.close()
 
 
